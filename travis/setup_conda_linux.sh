c6ca5a0bcf79869c353f50160bbf324afd3e5321
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-06-17 13:33:05 +0530

    Install all

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -11,13 +11,13 @@ else
     rm -rf $HOME/miniconda
     bash miniconda.sh -b -p $HOME/miniconda
 
-    export PATH="$HOME/miniconda/bin:$PATH"
 
-    # Install common Python dependencies
-    source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
 fi
 
 export PATH="$HOME/miniconda/bin:$PATH"
+
+# Install common Python dependencies
+source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
 
 
 if [[ $SETUP_XVFB == True ]]; then
